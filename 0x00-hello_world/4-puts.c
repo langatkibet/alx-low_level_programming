e275efab74bee2259303a64f9690a571168d8bb6
Kibet <40580034+langatkibet@users.noreply.github.com>
2022-06-30 13:40:04 -0700

    Puts not printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-/*Print out a statement*/
-int main(void)/*First program*/
+/**
+ * Print out a statement
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
