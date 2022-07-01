5296426600d6ab87a91df2b974d69439ff548d90
Kibet <40580034+langatkibet@users.noreply.github.com>
2022-07-01 13:46:05 -0700

    Numero

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main- entry point
+ *
+ *Return: 0 Success
+ */
+
+int main(void)
+{
+	int base;
+
+	for (base = 0; base <= 9; base++)
+	{
+		putchar(base);
+	}
+	putchar('\n');
+	return (0);
+}
