790641d8bf97a1f2a1e6f5798e1f701decec73f8
Kibet <40580034+langatkibet@users.noreply.github.com>
2022-07-01 08:33:58 -0700

    Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main -entry point
+ *
+ * Return: 0 sucess
+ */
+
+int main(void)
+{
+	char j;
+
+	for (j = "a"; j = "z"; j++)
+	{
+		putchar(j);
+		putchar('\n')
+	}
+}
