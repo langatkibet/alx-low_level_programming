43fb6d44ad44e0db02ceffce9f78614e5c72c0c5
Kibet <40580034+langatkibet@users.noreply.github.com>
2022-07-01 13:18:09 -0700

    Except

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main -entry point
+ *
+ *Return: 0 Success
+ */
+
+int main(void)
+{
+	char kenyor;
+
+	for (kenyor = 'a'; kenyor = 'z'; kenyor++)
+	{
+		if (kenyor != q && kenyor != e)
+			putchar(kenyor);
+	}
+	putchar('\n';)
+	return (0);
+}
