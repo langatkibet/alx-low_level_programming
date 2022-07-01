b88298e537d49481fb2d5b43a5ab7275e8022111
Kibet <40580034+langatkibet@users.noreply.github.com>
2022-07-01 13:27:46 -0700

    Singles

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main- entry point
+ *
+ *Return: 0 Success
+ */
+
+int main(void)
+{
+	int base;
+
+	for (base = 1; base = 10; base++)
+		printf("%d \n", base);
+	return (0);
+}
