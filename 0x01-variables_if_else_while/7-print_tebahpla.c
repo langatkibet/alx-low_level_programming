a33d97b60653d27b0cb1b3459b1d584b5b1b0b09
Kibet <40580034+langatkibet@users.noreply.github.com>
2022-07-01 13:52:32 -0700

    Seven

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main -entry point
+ *
+ * Return: 0 sucess
+ */
+
+int main(void)
+{
+	char jaa;
+
+	for (jaa = 'z'; jaa <= 'a'; jaa--)
+		putchar(jaa);
+	putchar('\n');
+	return (0);
+}
