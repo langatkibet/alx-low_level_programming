800d6f479417a9332bc44ac13dd9ad58038633e3
Kibet <40580034+langatkibet@users.noreply.github.com>
2022-07-01 14:39:17 -0700

    Last

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main -entry point
+ *
+ *Return: 0 Success
+ */
+
+int main(void)
+{
+	int y;
+
+	for (y = 48; y < 58; y++)
+	{
+		putchar(y);
+		if (y != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
