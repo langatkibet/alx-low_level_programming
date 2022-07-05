5e709464b7ae1ef7ab73cf2c4e6dc6fed52fde12
Kibet <40580034+langatkibet@users.noreply.github.com>
2022-07-05 12:55:30 -0700

    10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet ten times
+ * Return : void
+ */
+
+void print_alphabet_x10(void)
+{
+	int x = 1;
+	int yell;
+
+	for (x = 1; x <= 10; x++)
+	{
+		for (yell = 'a'; yell <= 'z'; yell++)
+			_putchar(yell);
+	}
+	_putchar('\n');
+}
