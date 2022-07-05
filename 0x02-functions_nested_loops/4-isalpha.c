2de0c52b8c9951783e68892553e26ad5bf4691ea
Kibet <40580034+langatkibet@users.noreply.github.com>
2022-07-05 13:27:11 -0700

    Four

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -9,12 +9,9 @@
 
 int _isalpha(int c)
 {
-	if (c > 64 && c < 91)
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 	{
-		if (c > 96 && c < 123)
-		{
-			return (1);
-		}
-	return (0);
+		return (1);
 	}
+	return (0);
 }
