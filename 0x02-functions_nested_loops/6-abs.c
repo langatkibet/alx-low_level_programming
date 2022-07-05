beca4022f21a5a1e02919604aece1560ba4c3575
Kibet <40580034+langatkibet@users.noreply.github.com>
2022-07-05 13:52:05 -0700

    Absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *
+ * _abs(int)- Absolute value
+ *
+ *Return: 0 Success
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		(n * -1);
+	}
+	else
+	{
+		return (n);
+	}
+	return (0);
+}
