a8021ef3e4dddc5c2eeacc6edbf2f96d772a94a1
Kibet <40580034+langatkibet@users.noreply.github.com>
2022-07-06 12:14:46 -0700

    Debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - test the positive/negative function
+ *
+ *Return: 0 Success
+ */
+
+int main(void)
+{
+	positive_or_negative(0);
+	return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,13 @@
+#ifndef _main_
+
+#define _main_
+
+#include <unistd.h>
+
+#include <stdio.h>
+
+
+
+void positive_or_negative(int i);
+
+#endif
