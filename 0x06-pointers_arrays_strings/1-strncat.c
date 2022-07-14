c6931c049661dbaa6a17fefc4daf3fa105bdb5c4
Kibet <40580034+langatkibet@users.noreply.github.com>
2022-07-14 03:58:47 -0700

    More arrays

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strncat - concatenate two str.
+ *@dest : variable
+ *@src : variable
+ *@n : the integer
+ * Return: Concatinated string.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+int i, a;
+for (i = 0; dest[i] != '\0'; i++)
+{
+continue;
+}
+for (a = 0; a < n && src[a] != '\0'; a++)
+{
+dest[i] += src[a];
+i++;
+}
+dest[i] += '\0';
+return (dest);
+}
